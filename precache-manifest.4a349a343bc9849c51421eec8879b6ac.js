38ffcc172f613c86b31a33ea12973011292669b7
hiMRK <sayhello.mrk@gmail.com>
2019-02-06 23:30:47 +0000

    Updates

--- precache-manifest.4a349a343bc9849c51421eec8879b6ac.js
+++ precache-manifest.4a349a343bc9849c51421eec8879b6ac.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/easy-multiply/static/js/runtime~main.1258a69e.js"
   },
   {
-    "revision": "f58e6298a8708882767b",
-    "url": "/easy-multiply/static/js/main.f58e6298.chunk.js"
+    "revision": "7cf84f78eeb2fefb83a4",
+    "url": "/easy-multiply/static/js/main.7cf84f78.chunk.js"
   },
   {
     "revision": "eae6a94a904fe33a71f8",
     "url": "/easy-multiply/static/js/1.eae6a94a.chunk.js"
   },
   {
-    "revision": "f58e6298a8708882767b",
+    "revision": "7cf84f78eeb2fefb83a4",
     "url": "/easy-multiply/static/css/main.b2c9617d.chunk.css"
   },
   {
-    "revision": "39faa4616101e0ab860df4e1108c3859",
+    "revision": "8fe8a27c973cb9c9b918dc85e49b3b6c",
     "url": "/easy-multiply/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/easy-multiply/precache-manifest.4bb7d2b2e22a8a82f0aad480944d6d6a.js"
+  "/easy-multiply/precache-manifest.4a349a343bc9849c51421eec8879b6ac.js"
 );
 
 workbox.clientsClaim();
