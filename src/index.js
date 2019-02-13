bc2fd3826c47d259746a263a765803b8b0e9e542
hiMRK <sayhello.mrk@gmail.com>
2019-02-13 22:48:22 +0000

    Remove Router/Bootstrap

--- src/index.js
+++ src/index.js
@@ -3,18 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.scss'
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-const ROUTING = (
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <>
-      <Switch>
-        <Route exact path="/" component={App} />
-      </Switch>
-    </>
-  </BrowserRouter> 
-);
-
-ReactDOM.render(ROUTING, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
